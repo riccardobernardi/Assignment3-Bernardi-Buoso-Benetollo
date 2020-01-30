d1b6e5c3b2a7aa2c6eabff0b7ef1f66094498218
Tommaso <tommaso.buoso@gmail.com>
2020-01-30 17:51:24 +0100

    Update einstein

---
+++ cmake-build-debug/CMakeFiles/mainTestLib.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mainTestLib.dir/mainTestLib.cpp.o"
+  "mainTestLib"
+  "mainTestLib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mainTestLib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
