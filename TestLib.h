c1667683119d7e283c2ace10030f608aaee0b088
riccardo bernardi <38067583+riccardobernardi@users.noreply.github.com>
2020-01-30 16:02:49 +0100

    added tests

--- TestLib.h
+++ TestLib.h
@@ -10,20 +10,6 @@
 #include <iostream>
 #include <zconf.h>
 
-/*void test(const std::function<void()>& callback, const std::string& name, int number){
-    pid_t pid = fork();
-
-    if (pid == 0) {
-        std::cout << "vvvv---------------TEST " << number << "-----------------------vvvv" << std::endl;
-        callback();
-        std::cout << std::endl << "***Concluso test [" << name << "]" << std::endl;
-        std::cout << "^^^^---------------TEST " << number << "-----------------------^^^^" << std::endl;
-        // std::cout << "***Errore al test [" << name << "]" << std::endl;
-    }else {
-        // std::cout << "sono il capo" << std::endl;
-    }
-}*/
-
 class Test{
 private:
     std::vector<std::function<void()>> _functions;
@@ -33,22 +19,22 @@ public:
     void launch_test(int x){
         if(x == -1){
             for(unsigned long i=0; i<_functions.size();++i){
-                pid_t pid = fork();
-                if (pid == 0) {
-                    std::cout << "vvvv---------------TEST " << i << "-----------------------vvvv" << std::endl;
-                    _functions[i]();
-                    std::cout << std::endl << "***Concluso test [" << _names[i] << "]" << std::endl;
-                    std::cout << "^^^^---------------TEST " << i << "-----------------------^^^^" << std::endl;
-                }else{}
+                //pid_t pid = fork();
+                //if (pid == 0) {
+                std::cout << "vvvv---------------TEST " << i << "-----------------------vvvv" << std::endl;
+                _functions[i]();
+                std::cout << std::endl << "***Concluso test [" << _names[i] << "]" << std::endl;
+                std::cout << "^^^^---------------TEST " << i << "-----------------------^^^^" << std::endl;
+                //}else{}
             }
         }else{
-            pid_t pid = fork();
-            if (pid == 0) {
-                std::cout << "vvvv---------------TEST " << x << "-----------------------vvvv" << std::endl;
-                _functions[x]();
-                std::cout << std::endl << "***Concluso test [" << _names[x] << "]" << std::endl;
-                std::cout << "^^^^---------------TEST " << x << "-----------------------^^^^" << std::endl;
-            }else{}
+            //pid_t pid = fork();
+            //if (pid == 0) {
+            std::cout << "vvvv---------------TEST " << x << "-----------------------vvvv" << std::endl;
+            _functions[x]();
+            std::cout << std::endl << "***Concluso test [" << _names[x] << "]" << std::endl;
+            std::cout << "^^^^---------------TEST " << x << "-----------------------^^^^" << std::endl;
+            //}else{}
         }
     }
     void add(const std::function<void()>& a, const std::string& name){
@@ -58,3 +44,4 @@ public:
 };
 
 #endif //TENSORLIB_TESTLIB_H
+
