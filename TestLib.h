9b0477504ca26620d8ff426d2b22a39621b1a492
riccardo bernardi <38067583+riccardobernardi@users.noreply.github.com>
2020-02-03 20:29:45 +0100

    Update TestLib.h

--- TestLib.h
+++ TestLib.h
@@ -51,12 +51,14 @@ public:
             }else{
                 std::cout << "Elapsed time in milliseconds : "
                           << std::chrono::duration_cast<std::chrono::milliseconds>(stop - init).count()
-                          << " ms" << std::endl;
+                          << " ms"" : " << std::chrono::duration_cast<std::chrono::microseconds>(stop - init).count()
+                          << " µs" << std::endl;
             }
         }else{
             std::cout << "Elapsed time in seconds : "
                       << std::chrono::duration_cast<std::chrono::seconds>(stop - this->init).count()
-                      << " sec" << std::endl;
+                      << " sec" << " : " << std::chrono::duration_cast<std::chrono::microseconds>(stop - init).count()
+                      << " µs" << std::endl;
         }
 
 
